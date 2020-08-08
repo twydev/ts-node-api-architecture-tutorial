5c6b81097e2afe98682e8c36d1d8367cccd92c02
twydev <39231273+twydev@users.noreply.github.com>
2020-08-08 14:55:58 +0800

    add base Entity class

---
+++ src/domain/entity.ts
@@ -0,0 +1,16 @@
+import { v4 as uuidv4 } from "uuid";
+
+// TODO: add domain events
+// import { EventEmitter } from 'events'; // NodeJS built-in library
+
+// decision: use of abstract class instead of interface because:
+//   1. we want to define method implementation and attributes common to all sub-classes.
+//   2. we should never instantiate the base class
+
+export abstract class Entity {
+  protected readonly id: string; // protected access, so that sub-class instances can access it.
+
+  constructor(id?: string) {
+    this.id = id ? id : uuidv4;
+  }
+}
