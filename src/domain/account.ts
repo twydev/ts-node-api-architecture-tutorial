6e6463c3a3fdcb00822f1a1ddc2f664e80d173ef
twydev <39231273+twydev@users.noreply.github.com>
2020-08-08 16:17:20 +0800

    Feature/domain layer (#5)
    * remove test folder
    
    * add base Entity class
    
    * extends Account from base Entity
    
    * add uuid dependencies
    
    * add unit test for Account domain model

---
+++ src/domain/account.ts
@@ -0,0 +1,28 @@
+import { Entity } from "./entity";
+
+export interface CreateAccountParams {
+  id?: string;
+  balance: number;
+}
+
+export class Account extends Entity {
+  // Decision: use a factory-style static method to create entity to allow validation of params
+  public static create(params: CreateAccountParams) {
+    if (Account.isValidBalance(params.balance))
+      return new Account(params.balance, params.id);
+    // TODO: need to handle invalid balance by throwing error
+  }
+
+  public static isValidBalance(balance: number) {
+    return balance >= 0;
+  }
+
+  public get balance(): number {
+    return this._balance;
+  }
+
+  // Decision: constructor is private to prevent unsafe creation of invalid Account states
+  private constructor(private _balance: number, id?: string) {
+    super(id);
+  }
+}
